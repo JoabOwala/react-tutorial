29acb21c013124ceb82c46852ae238e4ee88c061
JoabOwala <owalajoab@gmail.com>
2023-09-27 13:08:27 +0300

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./styles/Footer.css"
 
 function Footer(){
     return (
-        <>
+        <div className="foot">
         <small><p>© 2023 Owala development. All rights reserved.</p></small>
-        </>
+        </div>
     )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,15 @@ import "./styles/Header.css"
 function Header() {
     return (
         <header>
-            <nav className="nav">
+            <nav className="nav navbar navbar-light bg-light">
+                <a className="navbar-brand logi" href="#">
                 <img src="./logo192.png" alt="logo" className="nav-logo"/>
+                React
+                </a>
                 <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
         </header>
